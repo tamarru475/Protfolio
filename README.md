7a8c67de84b38900b2d1504f1f042c8f473f1ad0
Tamarru475 <tamarrubin475@gmail.com>
2023-03-08 13:39:46 +1300

    finish responsive and build

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Protfolio
+# Tamar Rubin - Protfolio
+
+**Features:**
+
+- My CV.
+- Introduction.
+- My projects.
+- My skills.
+- Contact details.
+
+**Tools used:**
+
+- Html5
+- SCSS
+- CSS
+- JavaScript
--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const theta = 2 * Math.PI / numImg;
 const prevBtn = carousel.querySelector('.projects__btn-prev');
 const nextBtn = carousel.querySelector('.projects__btn-next');
 
+const navButton = document.querySelector(".popupnav__button");
+const navBackground = document.querySelector(".popupnav__background");
+const navList = document.querySelector(".popupnav__nav");
+
 let currImg = 0;
 
 function slideCard() {
@@ -23,6 +27,22 @@ function next() {
     currImg++;
     slideCard();
 }
+
+function togglePopupNav(button, background, list) {
+    button.classList.toggle("popupnav__button_active");
+    background.classList.toggle("popupnav__background_active");
+    list.classList.toggle("popupnav__nav_active");
+}
+
+function closePopupNav(button, background, list) {
+    button.classList.remove("popupnav__button_active");
+    background.classList.remove("popupnav__background_active");
+    list.classList.remove("popupnav__nav_active");
+}
+
+
+navButton.addEventListener("click", () => togglePopupNav(navButton, navBackground, navList));
+navList.addEventListener("click", () => closePopupNav(navButton, navBackground, navList));
 
 prevBtn.addEventListener('click', () => prev());
 nextBtn.addEventListener('click', () => next());
